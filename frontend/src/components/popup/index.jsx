6c3b4f25632703954e295decca7b935d616d334a
masood <masoodacheampong@gmail.com>
2024-12-29 10:09:43 +0000

    frontend-change:modifying the frontend interface,

--- frontend/src/components/popup/index.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-
-function FormPopUp({ setShowLogin }) {
-  const [formState, setFormState] = useState("Login");
-  return <div>FormPopUp</div>;
-}
-
-export default FormPopUp;
--- frontend/src/pages/admin/foods/editFood.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function EditFood() {
-  return <div>EditFood</div>;
-}
-
-export default EditFood;
