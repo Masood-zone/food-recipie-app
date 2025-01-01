2964d81544f77e618cbf01ec6577b74bb3a3a691
masood <masoodacheampong@gmail.com>
2025-01-01 22:15:07 +0000

    replaced file uploader with filedropzone which displays selected phone.

--- frontend/src/pages/admin/pages/products/add/add-product-form.tsx
+++ frontend/src/pages/admin/pages/products/add/add-product-form.tsx
@@ -21,15 +21,9 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useState } from "react";
-import {
-  FileInput,
-  FileUploader,
-  FileUploaderContent,
-  FileUploaderItem,
-} from "@/components/shared/file-uploader/file-uploader";
-import { CloudUpload, Paperclip } from "lucide-react";
 import { useCreateProduct } from "@/pages/admin/services/queries/products/queries";
 import { useFetchAllCategoriesQuery } from "@/pages/admin/services/queries/category/queries";
+import FileUploadDropzone from "@/components/shared/file-uploader/file-dropzone";
 
 const formSchema = z.object({
   title: z.string().nonempty("Product title is required"),
@@ -47,6 +41,7 @@ export default function AddProductForm() {
     maxFiles: 1,
     maxSize: 1024 * 1024 * 4,
     multiple: false,
+    accept: { "image/*": [".jpg", ".jpeg", ".png"] },
   };
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -88,38 +83,19 @@ export default function AddProductForm() {
             <FormItem>
               <FormLabel>Select Product Photo</FormLabel>
               <FormControl>
-                <FileUploader
-                  value={files}
-                  onValueChange={setFiles}
+                <FileUploadDropzone
+                  value={files ?? undefined}
+                  onValueChange={(newFiles) => {
+                    setFiles(newFiles);
+                    if (newFiles && newFiles.length > 0) {
+                      const objectUrl = URL.createObjectURL(newFiles[0]);
+                      form.setValue("image", objectUrl); // Update form value
+                    } else {
+                      form.setValue("image", ""); // Remove form value
+                    }
+                  }}
                   dropzoneOptions={dropZoneConfig}
-                  className="relative bg-background rounded-lg p-2"
-                >
-                  <FileInput
-                    id="fileInput"
-                    className="outline-dashed outline-1 outline-slate-500"
-                  >
-                    <div className="flex items-center justify-center flex-col p-8 w-full ">
-                      <CloudUpload className="text-gray-500 w-10 h-10" />
-                      <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-                        <span className="font-semibold">Click to upload</span>
-                        &nbsp; or drag and drop
-                      </p>
-                      <p className="text-xs text-gray-500 dark:text-gray-400">
-                        SVG, PNG, JPG or GIF
-                      </p>
-                    </div>
-                  </FileInput>
-                  <FileUploaderContent>
-                    {files &&
-                      files.length > 0 &&
-                      files.map((file, i) => (
-                        <FileUploaderItem key={i} index={i}>
-                          <Paperclip className="h-4 w-4 stroke-current" />
-                          <span>{file.name}</span>
-                        </FileUploaderItem>
-                      ))}
-                  </FileUploaderContent>
-                </FileUploader>
+                />
               </FormControl>
               <FormDescription>Select a file to upload.</FormDescription>
               <FormMessage />
